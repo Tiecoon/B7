f709b7d9f9e233b021071c0470dd1b959637b26e
Luke Biery <luke@bierysbargainbarn.com>
2018-09-26 03:12:39 -0400

    initial ptrace code example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+extern crate libc;
+extern crate nix;
+
+use nix::sys::{ptrace, wait};
+use nix::unistd::{execve, fork, ForkResult, Pid};
+use std::ffi::CString;
+
 fn main() {
-    println!("Hello, world!");
+    match fork() {
+        Ok(ForkResult::Parent { child, .. }) => parent(child),
+        Ok(ForkResult::Child) => child(),
+        Err(_) => println!("Fork failed"),
+    }
+}
+
+fn child() {
+    assert!(ptrace::traceme().is_ok());
+    println!("CHILD: execve");
+    execve(
+        &CString::new("/bin/ls").expect("1"),
+        &[CString::new("a").expect("2")],
+        &[CString::new("a").expect("3")],
+    ).expect("CHILD: execve failed");
+    println!("CHILD: forking done");
+}
+
+fn parent(child: Pid) {
+    println!(
+        "Continuing execution in parent process, new child has pid: {}",
+        child
+    );
+    println!("PARENT: pid, {}", std::process::id());
+    wait::waitpid(child, Some(wait::WaitPidFlag::WSTOPPED)).expect("waitpid failed");
+    println!("PARENT: SLEEEPING\n\n\n");
+    std::thread::sleep(std::time::Duration::new(5, 0));
+    println!("PARENT: PTRACE CONTINUE\n\n\n");
+    ptrace::cont(child, None).expect("attach fail");
+    println!("PARENT: waitpid done");
 }
