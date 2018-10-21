bcfddabd713b7b5f1c77a00553d91033d7b899b2
Luke Biery <luke@bierysbargainbarn.com>
2018-10-20 20:37:35 -0400

    update to use processes over fork

--- src/main.rs
+++ src/main.rs
@@ -5,61 +5,39 @@
 
 extern crate libc;
 extern crate nix;
+extern crate spawn_ptrace;
 extern crate threadpool;
 
 use libc::{c_int, c_void, pid_t};
 use nix::sys::{ptrace, wait};
-use nix::unistd::{execve, fork, ForkResult, Pid};
-use std::ffi::CString;
+use nix::unistd::Pid;
+use spawn_ptrace::CommandPtraceSpawn;
+use std::process::Command;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 fn main() {
-    let mut x = 0;
-    match fork() {
-        Ok(ForkResult::Parent { child, .. }) => x = parent(child),
-        Ok(ForkResult::Child) => child(),
-        Err(_) => println!("Fork failed"),
-    }
-    println!("{}", x);
+    test();
 }
 
-fn child() {
-    assert!(ptrace::traceme().is_ok());
-    println!("CHILD: execve");
-    execve(
-        &CString::new("/bin/true").expect("1"),
-        &[CString::new("a").expect("2")],
-        &[CString::new("a").expect("3")],
-    )
-    .expect("CHILD: execve failed");
-    println!("CHILD: forking done");
-}
-
-fn parent(child: Pid) -> i64 {
-    println!(
-        "Continuing execution in parent process, new child has pid: {}",
-        child
-    );
-    println!("PARENT: pid, {}", std::process::id());
-    wait::waitpid(child, Some(wait::WaitPidFlag::WSTOPPED)).expect("waitpid failed");
-    // println!("PARENT: SLEEEPING\n\n\n");
-    // std::thread::sleep(std::time::Duration::new(5, 0));
-
-    // setup perf_event_open and return file descriptor to be read from
+fn proc_test() -> (i64) {
+    let child = Command::new("/bin/true")
+        .spawn_ptrace()
+        .expect("process creation failed");
     extern "C" {
         fn b77(input: pid_t) -> c_int;
     }
     let mut _test: c_int = 0;
     unsafe {
-        _test = b77(pid_t::from(child));
+        _test = b77(pid_t::from(child.id() as u16));
     }
 
     // continue execution
     println!("{}", _test);
-    println!("PARENT: PTRACE CONTINUE\n");
-    ptrace::cont(child, None).expect("ptrace fail");
-    wait::wait().expect("HUH");
+    let PID = Pid::from_raw(pid_t::from(child.id() as u16));
+    ptrace::cont(PID, None).expect("ptrace cont failed");
+
+    let _ = wait::waitpid(PID, Some(wait::WaitPidFlag::empty()));
 
     // read in number of unstructions
     let mut count: i64 = 0; // long long
@@ -68,41 +46,33 @@ fn parent(child: Pid) -> i64 {
         libc::read(_test, count_ptr, 8);
     }
 
-    println!("PARENT: instructions: {}", count);
-    println!("PARENT: waitpid done");
     return count;
 }
 
-#[test]
 fn test() {
     use std::sync::mpsc;
     use std::thread;
     use std::time::Duration;
     use threadpool::ThreadPool;
     let (tx, rx) = mpsc::channel();
-    let n_jobs = 10;
+    let n_jobs = 100;
     let n_workers = 20;
     let pool = ThreadPool::new(n_workers);
     for i in 0..n_jobs {
         let tx = tx.clone();
+        println!("queuing {}", i);
         pool.execute(move || {
             println!("exec {}", i);
-            let mut x: i64 = 0;
-            match fork() {
-                Ok(ForkResult::Parent { child, .. }) => x = parent(child),
-                Ok(ForkResult::Child) => child(),
-                Err(_) => println!("Fork failed"),
-            }
-            thread::sleep(Duration::from_secs(1));
-            tx.send(x)
+            let x = proc_test();
+            tx.send((x, i))
                 .expect("channel will be there waiting for the pool");
         });
-        println!("queued {}", i);
     }
 
-    for _ in 0..n_jobs {
-        let j = rx.recv().unwrap();
-        println!("Got: {:#6?} {:#20b}", j, j);
+    thread::sleep(Duration::from_secs(1));
+    for p in 0..n_jobs {
+        let (j, o) = rx.recv().unwrap();
+        println!("Got: {:#3} {:#3}/{:} {:#8} {:#30b}", o, p, n_jobs - 1, j, j);
     }
     println!("END");
 }
