fa8f2661d599b47a26269a327c01e461d0b89006
Kevin Phoenix <kevin@kphoenix.us>
2018-11-16 22:54:14 -0500

    Ignore bindings for rust-clippy

--- src/bindings.rs
+++ src/bindings.rs
@@ -2,5 +2,7 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(unknown_lints)]
+#![allow(clippy)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
