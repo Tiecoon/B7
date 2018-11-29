952d61cd0023da050a97d61b26cf5b06d1043a8d
Kevin Phoenix <kevin@kphoenix.us>
2018-11-29 17:34:50 -0500

    Move brute to own file

---
+++ src/brute.rs
@@ -0,0 +1,76 @@
+// use std::cmp::Ord;
+use std::fmt::{Debug, Display};
+use std::marker::Send;
+use std::sync::mpsc::channel;
+use threadpool::ThreadPool;
+
+use statistics;
+use b7tui;
+use generators::{Input, Generate};
+
+// can take out Debug trait later
+// Combines the generators with the instruction counters to deduce the next step
+pub fn brute<
+    G: Generate<I> + Display,
+    I: 'static + std::fmt::Display + Clone + Debug + Send + Ord,
+    B: b7tui::Ui,
+>(
+    path: &str,
+    repeat: u32,
+    gen: &mut G,
+    get_inst_count: fn(&str, &Input) -> i64,
+    terminal: &mut B,
+) {
+    // Loop until generator says we are done
+    loop {
+        // Number of threads to spawn
+        let n_workers = 8;
+        let mut num_jobs: i64 = 0;
+        let mut results: Vec<(I, i64)> = Vec::new();
+
+        let pool = ThreadPool::new(n_workers);
+        let (tx, rx) = channel();
+
+        // run each case of the generators
+        for inp_pair in gen.by_ref() {
+            num_jobs += 1;
+            let tx = tx.clone();
+            let test = String::from(path);
+            // give it to a thread to handle
+            pool.execute(move || {
+                let inp = inp_pair.1;
+                let mut avg: f64 = 0.0;
+                let mut count: f64 = 0.0;
+                for _ in 0..repeat {
+                    let inst_count = get_inst_count(&test, &inp);
+                    avg += inst_count as f64;
+                    count += 1.0;
+                    trace!("inst_count: {:?}", inst_count);
+                }
+                avg /= count;
+                let _ = tx.send((inp_pair.0, avg as i64));
+            });
+        }
+        // Track the minimum for stats later
+        let mut min: u64 = std::i64::MAX as u64;
+        // Get results from the threads
+        for _ in 0..num_jobs {
+            let tmp = rx.recv().unwrap();
+            if (tmp.1 as u64) < min {
+                min = tmp.1 as u64;
+            }
+            results.push(tmp);
+        }
+        results.sort();
+
+        terminal.update(&results, &min);
+
+        terminal.wait();
+
+        // inform generator of the result
+        let good_idx = statistics::find_outlier(results.as_slice());
+        if !gen.update(&good_idx.0) {
+            break;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -11,89 +11,22 @@ extern crate threadpool;
 extern crate tui;
 extern crate tui_logger;
 
-use b7tui::Ui;
-use clap::{App, Arg};
-use generators::*;
-use std::fs::File;
-use std::io::prelude::*;
-use std::sync::mpsc::channel;
-use threadpool::ThreadPool;
-
 pub mod b7tui;
 pub mod binary;
 pub mod bindings;
+pub mod brute;
 pub mod dynamorio;
 pub mod generators;
 pub mod perf;
 pub mod process;
 pub mod statistics;
 
-// can take out Debug trait later
-// Combines the generators with the instruction counters to deduce the next step
-fn brute<
-    G: Generate<I> + std::fmt::Display,
-    I: 'static + std::fmt::Display + Clone + std::fmt::Debug + std::marker::Send + std::cmp::Ord,
-    B: b7tui::Ui,
->(
-    path: &str,
-    repeat: u32,
-    gen: &mut G,
-    get_inst_count: fn(&str, &Input) -> i64,
-    terminal: &mut B,
-) {
-    // Loop until generator says we are done
-    loop {
-        // Number of threads to spawn
-        let n_workers = 8;
-        let mut num_jobs: i64 = 0;
-        let mut results: Vec<(I, i64)> = Vec::new();
-
-        let pool = ThreadPool::new(n_workers);
-        let (tx, rx) = channel();
-
-        // run each case of the generators
-        for inp_pair in gen.by_ref() {
-            num_jobs += 1;
-            let tx = tx.clone();
-            let test = String::from(path);
-            // give it to a thread to handle
-            pool.execute(move || {
-                let inp = inp_pair.1;
-                let mut avg: f64 = 0.0;
-                let mut count: f64 = 0.0;
-                for _ in 0..repeat {
-                    let inst_count = get_inst_count(&test, &inp);
-                    avg += inst_count as f64;
-                    count += 1.0;
-                    trace!("inst_count: {:?}", inst_count);
-                }
-                avg /= count;
-                let _ = tx.send((inp_pair.0, avg as i64));
-            });
-        }
-        // Track the minimum for stats later
-        let mut min: u64 = std::i64::MAX as u64;
-        // Get results from the threads
-        for _ in 0..num_jobs {
-            let tmp = rx.recv().unwrap();
-            if (tmp.1 as u64) < min {
-                min = tmp.1 as u64;
-            }
-            results.push(tmp);
-        }
-        results.sort();
-
-        terminal.update(&results, &min);
-
-        terminal.wait();
-
-        // inform generator of the result
-        let good_idx = statistics::find_outlier(results.as_slice());
-        if !gen.update(&good_idx.0) {
-            break;
-        }
-    }
-}
+use b7tui::Ui;
+use brute::brute;
+use clap::{App, Arg};
+use generators::*;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     // handle command line arguements
