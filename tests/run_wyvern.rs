30fc53e3b4f03840c6fe00bdb3ca6c7b944759c9
Aaron Hill <aa1ronham@gmail.com>
2019-01-29 17:13:29 -0500

    Use dynamorio in tests/run_wyvern.rs
    This allows the test to run on Travis CI, which lacks support for perf
    instruction counters

--- tests/run_wyvern.rs
+++ tests/run_wyvern.rs
@@ -1,5 +1,5 @@
 use b7::b7tui::Env;
-use b7::perf::get_inst_count;
+use b7::dynamorio;
 use b7::B7Opts;
 use std::collections::HashMap;
 use std::path::PathBuf;
@@ -7,18 +7,26 @@ use std::path::PathBuf;
 #[test]
 fn run_wyv() {
     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let mut dynpath = path.clone();
+
     path.push("tests");
     path.push("wyvern");
 
+    dynpath.push("dynamorio");
+    dynpath.push("build");
+
+
     let mut term = Env::new();
+    let mut vars = HashMap::new();
+    vars.insert("dynpath".to_string(), dynpath.to_string_lossy().into_owned());
 
     let mut opts = B7Opts::new(
         path.to_string_lossy().into_owned(),
         true,
         true,
-        get_inst_count,
+        dynamorio::get_inst_count,
         &mut term,
-        HashMap::new(),
+        vars,
     );
 
     let res = opts.run();
