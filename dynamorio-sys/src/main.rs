1c7222eb5d190209671b9227a3260b6598c0679a
Aaron Hill <aa1ronham@gmail.com>
2019-02-06 16:04:50 -0500

    Work on dynamorio_sys

--- dynamorio-sys/src/main.rs
+++ dynamorio-sys/src/main.rs
@@ -8,10 +8,21 @@ use std::ffi::{CString, CStr};
 #[no_mangle]
 pub static _USES_DR_VERSION_: c_int = bindings::_USES_DR_VERSION_;
 
+/*#[allow(non_upper_case_globals)]
+#[no_mangle]
+pub fn dr_client_main(id: client_id_t, argc: c_int, argv: *const *const c_char) {
+    println!("Hello from dr client: {}", id);
+}*/
+
+
 fn main() {
     //println!("Hello, world!");
     unsafe {
-        let app_name = CString::new("blah".to_string()).unwrap();
+        //let bin_path = CString::new(std::env::current_exe().unwrap().to_str().unwrap()).unwrap();
+        let bin_path = CString::new("/home/aaron/repos/B7/dynamorio-sys/dynamorio/build/api/bin/libinscount.so").unwrap();
+
+        let app_name = CString::new("/home/aaron/repos/B7/dynamorio-sys/test_bin").unwrap();
+        //let app_name = CString::new("/home/aaron/repos/B7/tests/wyvern".to_string()).unwrap();
         let app_name_ptr: *const c_char = app_name.as_ptr();
         let args = vec![CString::new("argone").unwrap(), CString::new("argtwo").unwrap()];
 
@@ -34,10 +45,27 @@ fn main() {
                             dr_path.as_ptr(), dr_operation_mode_t_DR_MODE_CODE_MANIPULATION, false as bool_, /* debug */
                             dr_platform_t_DR_PLATFORM_DEFAULT, opts_str.as_ptr());
 
+        println!("About to register client...");
+
+        let client_result = dr_register_client(raw_process, pid, false as bool_,
+                                                dr_platform_t_DR_PLATFORM_DEFAULT,
+                                                42 /* client id */,
+                                                0 /* priority */,
+                                                bin_path.as_ptr(),
+                                                opts_str.as_ptr());
+
+
+        println!("Registering client: {:?} {:?}", bin_path, client_result);
+
         let inject_res = dr_inject_process_inject(inject_data, false as bool_, std::ptr::null_mut());
         println!("Inject result: {}", inject_res);
         println!("Res: {} Registration: {} Pid: {} Process: {:?}", res, reg_result, pid, process);
 
         println!("Running: {}", dr_inject_process_run(inject_data));
+
+        println!("Waited: {}", dr_inject_wait_for_child(inject_data, 0));
+        let exit_code = dr_inject_process_exit(inject_data, false as bool_);
+
+        println!("Exit code: {}", exit_code);
     }
 }
---
+++ dynamorio-sys/test_bin.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Hello from test bin!\n");
+    return 89;
+}
