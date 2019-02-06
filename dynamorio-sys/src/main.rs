38c0f69e84e677cabba33247e24d6f735950cfdd
Aaron Hill <aa1ronham@gmail.com>
2019-02-06 00:54:33 -0500

    Include dynamorio version symbol in library

--- dynamorio-sys/src/main.rs
+++ dynamorio-sys/src/main.rs
@@ -1,11 +1,15 @@
 mod bindings;
 
 use bindings::*;
-use std::os::raw::{c_char, c_void};
+use std::os::raw::{c_char, c_void, c_int};
 use std::ffi::CString;
 
+#[allow(non_upper_case_globals)]
+#[no_mangle]
+pub static _USES_DR_VERSION_: c_int = bindings::_USES_DR_VERSION_;
+
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
     unsafe {
         let app_name = CString::new("blah".to_string()).unwrap();
         let app_name_ptr: *const c_char = app_name.as_ptr();
@@ -18,6 +22,7 @@ fn main() {
         let mut data_ptr = &mut data as *mut u8 as *mut c_void;
         let data_ptr_ptr = &mut data_ptr as *mut *mut c_void;
 
+        dr_app_setup();
         let res = dr_inject_process_create(app_name_ptr, raw_arg_ptr, data_ptr_ptr);
         println!("Res: {}", res);
     }
